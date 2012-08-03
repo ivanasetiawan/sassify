f1803eabb12e3fa55417fbf968930708d8c34bba
Ivana Setiawan <ivana@mangrove.nl>
2012-08-03 13:41:06 +0200

    Basic setup pushed

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-sassify
-=======
+Sassify!
 
-Sass one-o-one
+Sass one-o-one is created for anyone who's interested in learning/using Sass(http://sass-lang.com/).
+The goal is to explain briefly yet clearly about why should anyone use it and how to use it NOW.
+(Compass will be touched of course as well...)
+
+~ I. Setiawan
---
+++ config.rb
@@ -0,0 +1,27 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+# Path for images
+http_images_path = "/images/"
+http_generated_images_path = "/images/"
+
+css_dir = "public/css"
+sass_dir = "sass"
+images_dir = "public/images"
+javascripts_dir = "public/js"
+
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
